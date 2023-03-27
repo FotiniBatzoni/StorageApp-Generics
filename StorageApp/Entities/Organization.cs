26f6d3a19df1f0b7dfbc8f33fd284feaa688dcd5
fotini <f.batzoni@gmail.com>
2023-03-27 12:51:16 +0300

    Adding Organization class

---
+++ StorageApp/Entities/Organization.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace StorageApp.Entities
+{
+    public class Organization
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public override string ToString() => $"Id: {Id}, Name: {Name}"; 
+    }
+}
