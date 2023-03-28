dd752baafb5b0f9c408e28d91c044402a51085a0
fotini <f.batzoni@gmail.com>
2023-03-28 15:29:24 +0300

    Adding the class manager

---
+++ StorageApp/Entities/Manager.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace StorageApp.Entities
+{
+    public class Manager : Employee
+    {
+        public override string ToString()
+        {
+            return base.ToString() + " (Manager)";
+        }
+    }
+}
