f121f267131cfecd6aed9849f40f3c4c02369905
fotini <f.batzoni@gmail.com>
2023-03-29 15:29:08 +0300

    Create  a generic method

--- StorageApp/Program.cs
+++ StorageApp/Program.cs
@@ -50,10 +50,13 @@ namespace StorageApp
 
         private static void AddEmployees(IRepository<Employee> employeeRepository)
         {
-            employeeRepository.Add(new Employee { FirstName = "Julia" });
-            employeeRepository.Add(new Employee { FirstName = "Anna" });
-            employeeRepository.Add(new Employee { FirstName = "Tomas" });
-            employeeRepository.Save();
+            var employees = new[]
+            {
+                new Employee { FirstName = "Julia" },
+                new Employee { FirstName = "Anna" },
+                new Employee { FirstName = "Tomas" }
+            };
+            AddBatch(employeeRepository, employees);
         }
 
         private static void AddOrganazations(IRepository<Organization> organizationRepository)
@@ -67,13 +70,13 @@ namespace StorageApp
 
         }
 
-        private static void AddBatch(IRepository<Organization> organizationRepository, Organization[] organazations)
+        private static void AddBatch<T>(IWriteRepository<T> repository, T[] items)
         {
-            foreach (var item in organazations)
+            foreach (var item in items)
             {
-                organizationRepository.Add(item);
+                repository.Add(item);
             }
-            organizationRepository.Save();
+            repository.Save();
         }
     }
 }
